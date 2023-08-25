b0b41d855e13c7106ce12a8ba8bdac4a378899a5
Numi2 <33553832+Numi2@users.noreply.github.com>
2023-08-25 23:22:03 +0200

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,13 +1,13 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = 'b29eb951-228b-4302-aa3a-bbcdb2eb2346'
+export const API_KEY = 'app-i2LdxZMT9fJjd8Dkc6p7VeVr'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'eMedic - Medisinsk AI Assistent',
+  description: 'Møt eMedic - Norges ledende medisinske chatbot! Hos eMedic handler alt om å gjøre medisinsk informasjon tilgjengelig for alle. Enten du er en student som studerer medisin, en erfaren lege, eller bare en nysgjerrig sjel, har vi det du trenger. Vår AI-teknologi er designet for å hjelpe deg med å finne informasjonen du trenger, når du trenger den. Så bli med oss på denne spennende reisen mot en bedre informert verden!',
+  copyright: 'eMedic Proprietary Technology',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'nb',
 }
 
 export const isShowPrompt = false
